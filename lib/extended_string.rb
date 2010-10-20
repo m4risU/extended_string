ba5977041fbea7ae9dc8b298ac9197a70f516c32
Mariusz Wyrozębski <mariuszwyrozebski@gmail.com>
2010-10-21 00:39:50 +0200

    method

--- lib/extended_string.rb
+++ lib/extended_string.rb
@@ -1,3 +1,12 @@
-class String
-
+String.class_eval do
+  EXT_HOME_LETTERS = "letzsabBo"
+  EXT_BASE_LETTERS = "137254680"
+  def leet
+    copy_of_self = self.clone
+    symbols = EXT_HOME_LETTERS.chars.to_a.zip(EXT_BASE_LETTERS.chars.to_a)
+    symbols.each do |base,home|
+      copy_of_self.gsub!(base,home)
+    end
+    copy_of_self
+  end
 end
--- test/test_extended_string.rb
+++ test/test_extended_string.rb
@@ -1,7 +1,16 @@
 require 'helper'
 
 class TestExtendedString < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  should "change leet into 1337" do
+    assert_equal "1337", "leet".leet
   end
+
+  should "change alien into 41i3n" do
+    assert_equal "41i3n", "alien".leet
+  end
+
+#  should "probably rename this file and start testing for real" do
+#    flunk "hey buddy, you should probably rename this file and start testing for real"
+#  end
 end
